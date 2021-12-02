3ee4710aa9eda0cf9223851e7fedc2a30328c9f1
Igormcf <igormaciel82@gmail.com>
2021-12-02 16:20:23 -0300

    Adiciona requisitos 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  let area = (base * altura) / 2;
+  return area;
 }
 
 // Desafio 3
