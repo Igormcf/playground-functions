1024108e2f47da9408313cfe782dd59e145bfdab
Igormcf <igormaciel82@gmail.com>
2021-12-02 22:20:26 -0300

    Adiciona requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -85,11 +85,13 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string1) {
+  let novaFrase = string1.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5);
+  return novaFrase;
 }
-function decode() {
-  // seu código aqui
+function decode(string2) {
+  let novaFrase2 = string2.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  return novaFrase2;
 }
 
 module.exports = {
