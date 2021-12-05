8dd4d303e7712bfe8fbbabf956beb7b0473eef50
Igormcf <igormaciel82@gmail.com>
2021-12-05 00:29:26 -0300

    Adiciona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,11 +16,31 @@ function techList(technology, name) {
   }
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+  let ddd = numeros.join('').slice(0, 2);
+  let grupoNum1 = numeros.join('').slice(2, 7);
+  let grupoNum2 = numeros.join('').slice(7, );
+  let telefone = "(" + ddd + ")" + " " + grupoNum1 + "-" + grupoNum2;
+ 
+  if (numeros.length !== 11){
+    return "Array com tamanho incorreto.";
+  }
+  for (let index in numeros){
+    let contador = 0;
+    if (numeros[index] < 0 || numeros[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    for (let index2 in numeros){
+      if(numeros[index2] === numeros[index]){
+        contador = contador + 1;
+      }
+      if(contador >= 3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+  }
+  return telefone
 }
 
 // Desafio 12
